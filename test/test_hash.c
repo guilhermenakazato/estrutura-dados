1f6654155e7e35705ad76f343c4134b1237c2e87
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2024-04-08 11:21:12 -0400

    Update test_hash.c

--- test/test_hash.c
+++ test/test_hash.c
@@ -9,9 +9,32 @@ typedef struct {
     char cpf[11];
 } taluno;
 
+/*
+Caso queira fazer pras infos de um carro
+typedef struct {
+    char placa[10];
+    char modelo[30];
+    char marca[30];
+    char nome_motorista[30];
+} tcarro;
+
+char *get_key_placa(void *reg) {
+    return ((tcarro *)reg)->placa;
+
+    return (*((tcarro *) reg)).placa;
+    o mais interno -> type casting
+    o * do meio -> pega o conteúdo do ponteiro
+} 
+
+Sempre que trabalha com as structs, precisa de uma espécie de construtor, e é por isso que foi implementado o hash_constroi no hash.c. Basicamente, ele tenta fazer a alocação e você tem que zerar a estrutura com o calloc para determinar se a estrutura está vazia ou não. Com malloc, vai ter lixo e vai ter bagunça, pois o programa vai pensar que é um ponteiro e vai dar tudo errado.
+
+No construtor, você manda o endereço da variável para deleted pois você tem certeza que o calloc não vai pegar o endereço da variável.
+*/
+
 char *get_key(void *reg) {
     return ((taluno *)reg)->nome;
 }
+
 
 void *aloca_aluno(char *nome, char *cpf) {
     taluno *aluno = malloc(sizeof(taluno));
@@ -47,7 +70,7 @@ void test_search() {
     assert(inserir(&h, aloca_aluno("edson", "0123456789")) == EXIT_SUCCESS);
     assert(inserir(&h, aloca_aluno("takashi", "1123456789")) == EXIT_SUCCESS);
     assert(inserir(&h, aloca_aluno("matsubara", "2123456789")) == EXIT_SUCCESS);
-
+    
     aluno = buscar(h, "edson");
     assert(aluno->cpf[0] == '0');
     aluno = buscar(h, "takashi");
@@ -94,7 +117,7 @@ void test_remove() {
 
 int main(int argc, char *argv[]) {
     test_insere();
-    // test_search();
-    // test_remove();
+    test_search();
+    test_remove();
     return 0;
 }
